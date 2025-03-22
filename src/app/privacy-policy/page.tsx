5dbc422bcaff3ea40ee011e6d09466897a33eb43
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-03-22 02:37:34 +0100

    feat: implement privacy page

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -22,7 +22,7 @@ const Section: React.FC<SectionProps> = ({ title, children, isLast }) => (
       isLast ? "border-transparent" : "border-gray-500"
     } pb-5`}
   >
-    <h3 className="font-bold text-[28px]">{title}</h3>
+    <h3 className="font-bold text-2xl md:text-3xl lg:text-[28px]">{title}</h3>
     {children}
   </li>
 );
@@ -34,8 +34,8 @@ interface SubSectionProps {
 
 const SubSection: React.FC<SubSectionProps> = ({ title, content }) => (
   <div>
-    <h4 className="font-semibold text-[20px]">{title}</h4>
-    <p className="font-normal text-base leading-[150%]">{content}</p>
+    <h4 className="font-semibold text-lg md:text-xl lg:text-[20px]">{title}</h4>
+    <p className="font-normal text-sm md:text-base leading-[150%]">{content}</p>
   </div>
 );
 
@@ -129,23 +129,23 @@ const PrivacyPolicy = () => {
       <style>{customStyles}</style>
 
       <Navbar />
-      <div className="py-10">
-        <h1 className="font-semibold text-[64px] leading-none tracking-normal text-center pb-10 pt-24">
+      <div className="py-5 md:py-10 md:px-5">
+        <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-[64px] leading-none tracking-normal text-center pb-5 md:pb-10 pt-12 md:pt-24">
           Privacy Policy
         </h1>
-        <p className="font-light text-base leading-none tracking-normal text-center max-w-[593px] m-auto mb-12">
+        <p className="font-light text-sm sm:text-base leading-none tracking-normal text-center max-w-[90%] sm:max-w-[593px] m-auto mb-8 md:mb-12">
           FortiChain prioritizes user privacy and data security. The following
           policies outline our commitments and practices:
         </p>
-        <div className="bg-[#211A1D] rounded-[40px] p-10 border border-gray-500 max-w-[969px] m-auto">
-          <ol className="flex flex-col gap-5 list-decimal bold-numbers">
+        <div className="bg-[#211A1D] rounded-2xl md:rounded-[40px] p-7 md:p-10 border border-gray-500 max-w-[90%] sm:max-w-[80%] md:max-w-[969px] m-auto">
+          <ol className="flex flex-col gap-3 md:gap-5 list-decimal bold-numbers">
             {sections.map((section, index) => (
               <Section
                 key={index}
                 title={section.title}
                 isLast={index === sections.length - 1}
               >
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-2 md:gap-3">
                   {section.subsections.map((subsection, subIndex) => (
                     <SubSection
                       key={subIndex}
